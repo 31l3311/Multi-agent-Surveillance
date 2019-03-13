396ea9b44893742e1954f7fbc3decd6c9e7cbf9b
Annelot Pruyn <>
2019-03-13 12:59:56 +0100

    no message

--- src/agent/SurveillanceAgent.java
+++ src/agent/SurveillanceAgent.java
@@ -8,13 +8,13 @@ public class SurveillanceAgent extends Agent{
 	//speed per millisecond
 	private double baseSpeed = 0.0014;
 	private Point vector;
-	//defined as coordinates x,y to 2 decimal places
+	//defined as coordinates x,y in millimeters
 	private Point position;
 	//visual range 6 m
 	
 	public SurveillanceAgent(Point position) {
 		this.position = position;
-		vector = new Point(6, 6);
+		vector = new Point(3, 4);
 	}
 	
 	public void updatePos() {
@@ -33,8 +33,8 @@ public class SurveillanceAgent extends Agent{
 		System.out.println("U: " + u);
 		System.out.println(time*baseSpeed);
 		System.out.println("Vector: " + vector.x + ", " + vector.y);
-		position.x += 100*(u*vector.x);
-		position.y += 100*(u*vector.y);
+		position.x += Math.round(1000*(u*vector.x));
+		position.y += Math.round(1000*(u*vector.y));
 		System.out.println("Position: " + position.x + ", " + position.y);
 	}
 
