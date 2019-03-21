a70bbcf5d0721cd7f015c8b58411cf2097691759
Annelot Pruyn <>
2019-03-21 11:27:17 +0100

    no message

--- src/main/Run.java
+++ src/main/Run.java
@@ -21,7 +21,9 @@ public class Run {
 	private SurveillanceAgent agent = new SurveillanceAgent(new Point(0,0));;
 	private int time = 50;
 	private int[][] board = new int[200][200];
-
+	private ArrayList<SurveillanceAgent> agents;
+	private ArrayList<Intruder> intruders;
+	private ArrayList<Point> positions;
 
 	public void startTimer() {
 	GameTimer = new Timeline();
@@ -38,17 +40,19 @@ public class Run {
 		board[20][20] = 1;
 	}
 
-	public void check(ArrayList<Point> squares) {
+	public void check(ArrayList<Point> squares, int j) {
 		for(int i = 0; i<squares.size(); i++) {
 			//System.out.println(i + ",  " + squares.get(i).x + ", " + squares.get(i).y);
-			agent.updateMap(squares.get(i).x , squares.get(i).y, board[squares.get(i).x][squares.get(i).x]);
+			agents.get(j).updateMap(squares.get(i).x , squares.get(i).y, board[squares.get(i).x][squares.get(i).x]);
 		}
 	}
 
 
 	public void update() {
-		setBoard();
-		check(agent.update(time));
-
+		positions.clear();
+		for(int i = 0; i<agents.size(); i++) {
+		check(agents.get(i).update(time), i);
+		//agents.get(i).hear(agents);	
+		}
 	}
 }
